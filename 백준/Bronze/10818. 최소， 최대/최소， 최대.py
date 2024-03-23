ddc3ee00da96d4013cb3d731e7f997e476700541
Taein Jang <80192692+roblej@users.noreply.github.com>
2024-03-24 03:26:02 +0900

    [Bronze III] Title: 최소, 최대, Time: 468 ms, Memory: 144340 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -3,8 +3,8 @@ X=list(map(int,input().split()))
 max=X[0]
 min=X[0]
 for i in range (N):
-    if(max<X[i]):
+    if(max<=X[i]):
         max=X[i]
-    if(min>X[i]):
+    if(min>=X[i]):
         min=X[i]
 print(min,max)
